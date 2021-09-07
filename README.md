8d485e9bef43863e67fed940e4ae727ea0f241ec
ravenolf <benahmed@soramitsu.co.jp>
2021-09-07 08:54:30 -0400

    update main controller
    Signed-off-by: ravenolf <benahmed@soramitsu.co.jp>

--- README.md
+++ README.md
@@ -3,12 +3,15 @@
 ## Overview
 
 This micro-service is responsible for the following action within the dummy project:
-- Responding to get requests 
+
+- Responding to get requests
 - Securing the access through username/password authentication (see Configuration section)
+- Returning a list of access date to the /do-something-persistent endpoint
 
 ## System requirements
 
 This micro-service requires the following resources in kubernetes format:
+
 ```yaml
 resources:
   requests:
@@ -22,21 +25,24 @@ resources:
 ## Build, test & run
 
 ### Build
+
 This project is written using Gradle and Java 11.
 
 ### Run unit tests
+
 ```bash
 ./gradlew test
 ```
 
 ### Run integration tests
+
 ```bash
 ./gradlew test
 ```
 
 ### Run
 
-After that run container
+After that run the application
 
 ```bash 
 ./gradlew run
@@ -44,18 +50,22 @@ After that run container
 
 ## Configuration parameters
 
-
 File with parameters - devops-interview-app/src/main/resources/application.yml
 
 Environment variables for configuring:
 
 - `SERVER_PORT` :int: port for the main http server
+- `APPLICATION_FILE_PATH` :int: path of the file created by the application
 - `SPRING_SECURITY_USER_NAME` :string: username for the authentication
 - `SPRING_SECURITY_USER_PASSWORD` :string: password for the authentication
 
 ## Endpoints
-By default, the application binds its http server on port `8080` and its monitoring port on `8081`.
-It exposes three main endpoints:
-- `GET /api/v1/do-something` on port `8080`: business logic endpoint
+
+By default, the application binds its http server on port `8080` and its monitoring port on `8081`. It exposes three
+main endpoints:
+
+- `GET /api/v2/do-something` on port `8080`: returns a string
+- `GET /api/v2/do-something-persistent` on port `8080`: appends the access date on a file and returns the content of the
+  file
 - `GET /actuator/health/readiness` on port `8081`: readiness probe for the application
 - `GET /actuator/health/liveness` on port `8081`: liven probe for the application
--- src/main/java/co/jp/soramitsu/devopsinterviewapp/MainController.java
+++ src/main/java/co/jp/soramitsu/devopsinterviewapp/MainController.java
@@ -1,20 +1,83 @@
 package co.jp.soramitsu.devopsinterviewapp;
 
-import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
 @Controller
-@RequestMapping("/api/v1")
-@AllArgsConstructor
+@RequestMapping("/api/v2")
 public class MainController {
+
+    @Value("${application.file-path}")
+    private String filePath;
 
     @GetMapping("/do-something")
     public @ResponseBody
     ResponseEntity<String> doSomething() {
         return ResponseEntity.ok("I'm doing something");
     }
+
+    @GetMapping("/do-something-persistent")
+    public @ResponseBody
+    ResponseEntity<List<String>> doSomethingPersistent() {
+        try {
+            initFile();
+
+            String current = readFile();
+            writeOnFile(current + "\n" + getCurrentDate());
+
+            List<String> accessTimes = Arrays.stream(
+                            readFile().split("\n"))
+                    .filter((s) -> s.length() > 0)
+                    .collect(Collectors.toList());
+
+            log.info("Successful access to " + filePath);
+            return ResponseEntity.ok(accessTimes);
+        } catch (IOException e) {
+            log.error(e.getMessage());
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+
+    private boolean initFile() throws IOException {
+        File file = new File(filePath);
+        return file.createNewFile();
+    }
+
+    private String getCurrentDate() {
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
+        return sdf.format(cal.getTime());
+    }
+
+    private void writeOnFile(String string) throws IOException {
+        FileOutputStream outputStream = new FileOutputStream(filePath);
+        byte[] strToBytes = string.getBytes();
+        outputStream.write(strToBytes);
+        outputStream.close();
+    }
+
+    private String readFile() throws IOException {
+        FileInputStream inputStream = new FileInputStream(filePath);
+        byte[] bytes = inputStream.readAllBytes();
+        inputStream.close();
+        return new String(bytes, StandardCharsets.UTF_8);
+    }
+
 }
